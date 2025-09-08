c2b1aa2e1bcb24198b16573d91696a33fc345dbd
emmabeni27 <larisabeni@mail.com>
2025-09-08 18:55:11 -0300

    corrigiendo build

--- src/test/java/util/UnexpectedMessageException.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class UnexpectedMessageException extends RuntimeException {
-    public UnexpectedMessageException(String message) {
-        super("Message: \"" + message + "\" was unexpected." );
-    }
-}
